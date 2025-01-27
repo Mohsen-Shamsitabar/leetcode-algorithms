ba418baaebd1b7201462a6d7fc5b76c9dcc30e74
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-01-27 17:11:38 +0330

    daily commit

--- src/problems/valid-parentheses/validParentheses.ts
+++ src/problems/valid-parentheses/validParentheses.ts
@@ -1,5 +1,65 @@
-const validParentheses = () => {
-  return;
+const _validParentheses = (s: string): boolean => {
+  const parenthesesMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
+  };
+
+  const opens = Object.keys(parenthesesMap);
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (opens.includes(char)) {
+      stack.push(char);
+      continue;
+    }
+
+    if (stack.length === 0) return false;
+
+    const lastOppened = stack.pop()!;
+
+    if (char !== parenthesesMap[lastOppened as keyof typeof parenthesesMap]) {
+      return false;
+    }
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
 };
 
-export default validParentheses;
+const validParentheses2 = (s: string): boolean => {
+  const parenthesesMap: Map<string, string> = new Map([
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"]
+  ]);
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (parenthesesMap.has(char)) {
+      stack.push(char);
+      continue;
+    }
+
+    if (stack.length === 0) return false;
+
+    const lastOppened = stack.pop()!;
+
+    if (char !== parenthesesMap.get(lastOppened)) {
+      return false;
+    }
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
+};
+
+export default validParentheses2;
