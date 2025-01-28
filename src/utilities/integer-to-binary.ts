2ac846a27c89b85cb73b9342f22b4e4a31f7467a
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-01-28 21:40:08 +0330

    add new utility functions

---
+++ src/utilities/integer-to-binary.ts
@@ -0,0 +1,18 @@
+const integerToBinary = (integerNumber: number) => {
+  let result = "";
+
+  let x = integerNumber;
+  while (x >= 2) {
+    const remainder = x % 2;
+    x = Math.floor(x / 2);
+    result = `${remainder}` + result;
+  }
+
+  if (x) {
+    result = `${x}` + result;
+  }
+
+  return result;
+};
+
+export default integerToBinary;
