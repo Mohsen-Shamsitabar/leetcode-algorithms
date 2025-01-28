e3e007169ad6b7436b83844963d7a83c603f3f30
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-01-28 21:39:51 +0330

    solve `addBinary` problem

--- src/problems/index.ts
+++ src/problems/index.ts
@@ -1,3 +1,4 @@
+export { default as addBinary } from "./add-binary";
 export { default as romanToInteger } from "./roman-to-integer";
 export { default as twoSum } from "./two-sum";
 export { default as validParentheses } from "./valid-parentheses";
