6c32332f7119962be0d748c4085c38f712a740e7
Shinmo <85071377+Mohsen-Shamsitabar@users.noreply.github.com>
2025-02-02 23:21:33 +0330

    Daily commit (#7)
    * solve `mergeSortedLists`
    
    * solve `longestCommonPrefix`

---
+++ src/problems/longest-common-prefix/description.md
@@ -0,0 +1,24 @@
+### Longest Common Prefix (easy)
+
+> https://leetcode.com/problems/longest-common-prefix/description/
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string `""`.
+
+##### Example1:
+
+> **Input:** `strs = ["flower","flow","flight"]`.
+> **Output:** `"fl"`.
+
+##### Example2:
+
+> **Input:** strs = `["dog","racecar","car"]`.
+> **Output:** `""`.
+> **Explanation:** There is no common prefix among the input strings.
+
+##### Constraints:
+
+- `1 <= strs.length <= 200`
+- `0 <= strs[i].length <= 200`
+- `strs[i]` consists of only lowercase English letters if it is non-empty.
---
+++ src/problems/longest-common-prefix/longestCommonPrefix.ts
@@ -0,0 +1,44 @@
+const _firstApproach = (strs: string[]): string => {
+  let result = strs[0];
+
+  if (strs.length === 1) return result;
+
+  for (const str of strs) {
+    if (!str.length || !result.length) return "";
+
+    for (let i = 0; i < result.length; i++) {
+      if (i >= str.length) {
+        result = result.slice(0, i);
+        break;
+      }
+
+      const resultChar = result[i];
+      const strChar = str[i];
+
+      if (resultChar !== strChar) {
+        result = result.slice(0, i);
+      }
+    }
+  }
+
+  return result;
+};
+
+const secondApproach = (strs: string[]): string => {
+  const sorted = strs.sort((a, b) => (a < b ? -1 : 1));
+
+  const output = [];
+  const firstword = sorted[0];
+  const lastword = sorted[sorted.length - 1];
+  for (let i = 0; i < firstword.length; i++) {
+    if (firstword[i] == lastword[i]) {
+      output.push(firstword[i]);
+    } else {
+      break;
+    }
+  }
+
+  return output.join("");
+};
+
+export default secondApproach;
