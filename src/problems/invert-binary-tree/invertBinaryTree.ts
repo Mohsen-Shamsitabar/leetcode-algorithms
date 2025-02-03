23f194f126c67380ce72f0206cda66dedfcc31e4
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-03 16:49:33 +0330

    init

---
+++ src/problems/invert-binary-tree/invertBinaryTree.ts
@@ -0,0 +1,7 @@
+import type { TreeNode } from "../../types";
+
+const invertTree = (root: TreeNode | null): TreeNode | null => {
+  return null;
+};
+
+export default invertTree;
---
+++ src/problems/reverse-linked-list/reverseLinkedList.ts
@@ -0,0 +1,7 @@
+import type { ListNode } from "../../types";
+
+const reverseList = (head: ListNode | null): ListNode | null => {
+  return null;
+};
+
+export default reverseList;
