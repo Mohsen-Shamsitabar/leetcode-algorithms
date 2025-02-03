e56fe2d1614f2148cdf39bf6e88ec9877dd4fbb6
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-03 17:40:52 +0330

    solve `reverseLinkedList`

--- src/problems/reverse-linked-list/reverseLinkedList.ts
+++ src/problems/reverse-linked-list/reverseLinkedList.ts
@@ -1,7 +1,33 @@
 import type { ListNode } from "../../types";
 
-const reverseList = (head: ListNode | null): ListNode | null => {
-  return null;
+const traverseApproach = (head: ListNode | null): ListNode | null => {
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = head;
+
+  while (curr !== null) {
+    const nextNode = curr.next;
+
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  return prev;
 };
 
-export default reverseList;
+const _recursiveApproach = (head: ListNode | null): ListNode | null => {
+  if (!head) return head;
+
+  const reverse = (curr: ListNode, parent: ListNode | null): ListNode => {
+    const next = curr.next;
+
+    curr = { val: curr.val, next: parent };
+
+    if (!next) return curr;
+    return reverse(next, curr);
+  };
+
+  return reverse(head, null);
+};
+
+export default traverseApproach;
