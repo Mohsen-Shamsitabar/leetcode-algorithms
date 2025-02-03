60199750420b891a8f6428b1834cdfd41df9ba25
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-03 17:52:46 +0330

    solve `invertBinaryTree`

--- src/problems/invert-binary-tree/invertBinaryTree.ts
+++ src/problems/invert-binary-tree/invertBinaryTree.ts
@@ -1,7 +1,34 @@
 import type { TreeNode } from "../../types";
 
-const invertTree = (root: TreeNode | null): TreeNode | null => {
-  return null;
+const _firstApproach = (root: TreeNode | null): TreeNode | null => {
+  if (!root) return null;
+
+  const switchNodes = (root: TreeNode | null): void => {
+    if (!root?.left && !root?.right) return;
+
+    const left = root.left;
+    const right = root.right;
+
+    root.left = right;
+    root.right = left;
+
+    switchNodes(root.left);
+    switchNodes(root.right);
+  };
+
+  switchNodes(root);
+  return root;
 };
 
-export default invertTree;
+const secondApproach = (root: TreeNode | null): TreeNode | null => {
+  if (!root) return null;
+
+  [root.left, root.right] = [root.right, root.left];
+
+  secondApproach(root.left);
+  secondApproach(root.right);
+
+  return root;
+};
+
+export default secondApproach;
