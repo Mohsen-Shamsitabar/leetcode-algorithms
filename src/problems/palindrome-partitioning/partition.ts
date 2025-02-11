eed85f0b7fa6122f77dd51e3b60364cd51a81d54
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-11 15:19:42 +0330

    solve `palindromePartitions`

--- src/problems/palindrome-partitioning/partition.ts
+++ src/problems/palindrome-partitioning/partition.ts
@@ -1,3 +1,44 @@
-const partition = (s: string): string[][] => {};
+/**
+  Time Complexity: O(n * 2^n).
+  Space Complexity: O(n^2).
+ */
+const backTrackingApproach = (s: string): string[][] => {
+  const result: string[][] = [];
+  const solution: string[] = [];
 
-export default partition;
+  const isPalindrome = (str: string): boolean => {
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+      if (str[left] !== str[right]) return false;
+
+      left++;
+      right--;
+    }
+
+    return true;
+  };
+
+  const backtrack = (start: number): void => {
+    if (start === s.length) {
+      result.push([...solution]);
+      return;
+    }
+
+    for (let end = start + 1; end <= s.length; end++) {
+      const substr = s.substring(start, end);
+
+      if (isPalindrome(substr)) {
+        solution.push(substr);
+        backtrack(end);
+        solution.pop();
+      }
+    }
+  };
+
+  backtrack(0);
+  return result;
+};
+
+export default backTrackingApproach;
