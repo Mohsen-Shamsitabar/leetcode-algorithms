5a9657c7f390d4d42acbcf942f31e4b2be3ae175
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-14 13:09:13 +0330

    solve `maxProfit`

--- src/problems/buy-and-sell-stock-with-transaction-fee/maxProfit.ts
+++ src/problems/buy-and-sell-stock-with-transaction-fee/maxProfit.ts
@@ -1,5 +1,16 @@
 const maxProfit = (prices: number[], fee: number): number => {
-  return 0;
+  let cash = 0;
+  let hold = -Infinity;
+
+  for (const stockPrice of prices) {
+    // hold the cash, or sell stock.
+    cash = cash >= hold + stockPrice - fee ? cash : hold + stockPrice - fee;
+
+    // hold stock or buy stock. (we buy the first stock)
+    hold = hold >= cash - stockPrice ? hold : cash - stockPrice;
+  }
+
+  return cash;
 };
 
 export default maxProfit;
