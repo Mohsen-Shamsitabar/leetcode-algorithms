4341f06d5b88828aa465a404fab35d9f5e0620df
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-22 13:40:54 +0330

    solve `productExceptSelf`

---
+++ src/problems/product-except-self/productExceptSelf.ts
@@ -0,0 +1,23 @@
+const productExceptSelf = (nums: number[]): number[] => {
+  const numsLength = nums.length;
+
+  const result = new Array<number>(numsLength).fill(1);
+
+  let product = 1;
+
+  for (let i = 0; i < numsLength; i++) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+
+  product = 1;
+
+  for (let i = numsLength - 1; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+
+  return result;
+};
+
+export default productExceptSelf;
