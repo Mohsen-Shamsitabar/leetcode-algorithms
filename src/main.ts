5b6835717dad3614524352cd32a3559d6f6ec18b
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-26 14:40:25 +0330

    refactor root scope

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "normalize.css";
+import { swapPairs } from "./problems";
+import { test1 } from "./problems/swap-nodes-in-pair/test-cases";
 import "./style.css";
 
 const root = document.querySelector<HTMLDivElement>("#app");
@@ -6,3 +8,5 @@ const root = document.querySelector<HTMLDivElement>("#app");
 if (!root) {
   throw new Error("No Root element detected!");
 }
+
+console.log(swapPairs(test1));
