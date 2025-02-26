094d96753a4f2acb11c607b736b8a093618c6df9
Shinmo <85071377+Mohsen-Shamsitabar@users.noreply.github.com>
2025-02-26 14:45:12 +0330

    Daily commit (#28)
    * rename `informations` to `notes`
    
    * fix linter
    
    * refactor whitespace
    
    * add lint script
    
    * refactor root scope
    
    * fix typo
    
    * solve `swapPairs`

--- src/problems/binary-tree-level-order-traversal/levelOrderTraversal.ts
+++ src/problems/binary-tree-level-order-traversal/levelOrderTraversal.ts
@@ -1,7 +1,7 @@
 import type { TreeNode } from "../../types";
 /*
-*  My approach is NOT CORRECT!
-*/
+ *  My approach is NOT CORRECT!
+ */
 const _myApproach = (root: TreeNode | null): number[][] => {
   if (!root) return [];
 
