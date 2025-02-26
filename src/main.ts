23f55fc2cd0350b746ff064b52678b33b6114157
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-02-26 14:44:18 +0330

    fix typo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import "normalize.css";
-import { swapPairs } from "./problems";
-import { test1 } from "./problems/swap-nodes-in-pair/test-cases";
 import "./style.css";
 
 const root = document.querySelector<HTMLDivElement>("#app");
@@ -8,5 +6,3 @@ const root = document.querySelector<HTMLDivElement>("#app");
 if (!root) {
   throw new Error("No Root element detected!");
 }
-
-console.log(swapPairs(test1));
