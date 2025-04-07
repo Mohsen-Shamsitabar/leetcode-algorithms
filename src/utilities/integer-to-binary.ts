1f3986637d628d3c652e88c9527894b60faaf7cd
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-04-07 13:39:44 +0330

    update `integerToBinary.ts`

--- src/utilities/integer-to-binary.ts
+++ src/utilities/integer-to-binary.ts
@@ -1,20 +1,22 @@
-const integerToBinary = (integerNumber: number) => {
-  let result = "";
+// const integerToBinary = (integerNumber: number) => {
+//   let result = "";
 
-  let x = integerNumber;
+//   let x = integerNumber;
 
-  while (x >= 2) {
-    const remainder = x % 2;
+//   while (x >= 2) {
+//     const remainder = x % 2;
 
-    x = Math.floor(x / 2);
-    result = `${remainder}` + result;
-  }
+//     x = Math.floor(x / 2);
+//     result = `${remainder}` + result;
+//   }
 
-  if (x) {
-    result = `${x}` + result;
-  }
+//   if (x) {
+//     result = `${x}` + result;
+//   }
 
-  return result;
-};
+//   return result;
+// };
+
+const integerToBinary = (integerNumber: number) => integerNumber.toString(2);
 
 export default integerToBinary;
