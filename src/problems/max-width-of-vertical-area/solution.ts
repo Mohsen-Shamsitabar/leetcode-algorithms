09a4610bb2bf4b7502828e9d348852312dd24bbd
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-05-10 11:27:34 +0330

    fix type

--- src/problems/max-width-of-vertical-area/solution.ts
+++ src/problems/max-width-of-vertical-area/solution.ts
@@ -2,13 +2,13 @@
  * Time = **`O(n log n)`**
  * Space = **`O(1)`** / **`O(n)`**
  */
-const solution = (points: [number, number][]): number => {
-  points.sort((a, b) => a[0] - b[0]);
+const solution = (points: number[][]): number => {
+  points.sort((a, b) => a[0]! - b[0]!);
 
   let result = 0;
 
   for (let i = 1; i < points.length; i++) {
-    result = Math.max(Math.abs(points[i - 1]![0] - points[i]![0]), result);
+    result = Math.max(Math.abs(points[i - 1]![0]! - points[i]![0]!), result);
   }
 
   return result;
